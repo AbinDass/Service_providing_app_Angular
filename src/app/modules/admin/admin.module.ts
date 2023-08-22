13f0fc3f17dd3ed968d2a651f6ff7631294d00a6
AbinDass <abindas350@gmail.com>
2023-08-22 07:03:22 +0000

    navbar done

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -5,12 +5,14 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { AdminComponent } from './admin.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './pages/layout/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    LoginComponent
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
