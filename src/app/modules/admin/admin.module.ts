d155adecb0841d8713d89690207f4e603503a839
AbinDass <abindas350@gmail.com>
2023-08-22 07:15:27 +0000

    set up and cleared conflict completed

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -5,12 +5,16 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { AdminComponent } from './admin.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './pages/layout/navbar/navbar.component';
+import { DashboadComponent } from './pages/dashboad/dashboad.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    LoginComponent
+    LoginComponent,
+    DashboadComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
